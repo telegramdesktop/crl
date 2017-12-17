a15c983eb57b818b33f779bc2c3a6250523cf417
John Preston <johnprestonmail@gmail.com>
2017-12-17 16:27:22 +0400

    Test performance with multiple serial queues.
    Qt thread pool implementation is killed here:
    
    0.6 seconds on WinAPI concurrency vs 27 seconds on Qt.

--- src/test.cpp
+++ src/test.cpp
@@ -3,6 +3,7 @@
 #include <crl/crl.h>
 #include <iostream>
 #include <chrono>
+#include <numeric>
 
 void testOutput(crl::queue *queue) {
 	for (auto i = 0; i != 1000; ++i) {
@@ -17,30 +18,49 @@ void testOutput(crl::queue *queue) {
 	}
 }
 
+constexpr auto kQueueCount = 8;
+
+struct CacheLine {
+	static constexpr auto kSize = 128; // To be sure.
+
+	int value;
+	char padding[kSize - sizeof(int)];
+};
+
+int operator+(int already, const CacheLine &a) {
+	return already + a.value;
+}
+
 std::atomic<int> added = 0;
-int testCounting(crl::queue *queue) {
-	auto result = 0;
-	for (auto i = 0; i != 10000000; ++i) {
-		queue->async([i, &result] { ++result; });
+int testCounting(crl::queue (&queues)[kQueueCount]) {
+	CacheLine result[kQueueCount] = { 0 };
+	for (auto i = 0; i != 100000; ++i) {
+		for (auto j = 0; j != kQueueCount; ++j) {
+			queues[j].async([&result, j] { ++result[j].value; });
+		}
 		if ((i % 10000) == 9999) {
 			crl::async([i] { ++added; });
-			queue->sync([i, &result] { ++result; });
+			const auto j = ((i + 1) / 10000) % kQueueCount;
+//			queues[j].sync([j, &result] { ++result[j].value; });
 		}
 	}
-	return result + added;
+	for (auto j = 0; j != kQueueCount; ++j) {
+		queues[j].sync([&result, j] { ++result[j].value; });
+	}
+	return std::accumulate(std::begin(result), std::end(result), 0) + added;
 }
 
 int main() {
-	crl::queue testQueue;
-//	testOutput(&testQueue);
+	crl::queue testQueue[kQueueCount];
+//	testOutput(&testQueue[0]);
 	for (int i = 0; i != 5; ++i) {
 		auto start_time = std::chrono::high_resolution_clock::now();
-		auto result = testCounting(&testQueue);
+		auto result = testCounting(testQueue);
 		auto end_time = std::chrono::high_resolution_clock::now();
 		auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
 		std::cout << "Time: " << ms.count() / 1000. << " (" << result << ")" << std::endl;
 	}
-	std::cout << "Hello, World!" << std::endl;
+	std::cout << "Finished." << std::endl;
 	int a = 0;
 	std::cin >> a;
 	return 0;
