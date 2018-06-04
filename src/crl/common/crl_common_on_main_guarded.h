f893c36427993e22181c8b3ec382dedb5563dc08
John Preston <johnprestonmail@gmail.com>
2018-06-04 18:33:12 +0300

    Allow using guarded callables externally.

--- src/crl/common/crl_common_on_main_guarded.h
+++ src/crl/common/crl_common_on_main_guarded.h
@@ -24,21 +24,70 @@ namespace crl {
 template <typename T, typename Enable = void>
 struct guard_traits;
 
+template <typename Callable>
+struct deduce_call_type_traits {
+	using type = decltype(&Callable::operator());
+};
+
+template <typename Callable>
+using deduced_call_type = typename deduce_call_type_traits<
+	std::decay_t<Callable>>::type;
+
+template <typename Guard, typename Callable>
+class guarded_wrap {
+public:
+	using ClearCallable = std::decay_t<Callable>;
+	using GuardTraits = guard_traits<std::decay_t<Guard>>;
+	using GuardType = decltype(GuardTraits::create(std::declval<Guard>()));
+
+	guarded_wrap(Guard &&object, Callable &&callable)
+	: _guard(GuardTraits::create(std::forward<Guard>(object)))
+	, _callable(std::forward<Callable>(callable)) {
+	}
+
+	template <
+		typename ...OtherArgs,
+		typename Return = decltype(
+			std::declval<ClearCallable>()(std::declval<OtherArgs>()...))>
+	Return operator()(OtherArgs &&...args) {
+		return GuardTraits::check(_guard)
+			? _callable(std::forward<OtherArgs>(args)...)
+			: Return();
+	}
+
+	template <
+		typename ...OtherArgs,
+		typename Return = decltype(
+			std::declval<ClearCallable>()(std::declval<OtherArgs>()...))>
+	Return operator()(OtherArgs &&...args) const {
+		return GuardTraits::check(_guard)
+			? _callable(std::forward<OtherArgs>(args)...)
+			: Return();
+	}
+
+private:
+	GuardType _guard;
+	ClearCallable _callable;
+
+};
+
+template <typename Guard, typename Callable>
+struct deduce_call_type_traits<guarded_wrap<Guard, Callable>> {
+	using type = deduced_call_type<Callable>;
+};
+
 template <
 	typename Guard,
 	typename Callable,
 	typename GuardTraits = guard_traits<std::decay_t<Guard>>,
 	typename = std::enable_if_t<
 		sizeof(GuardTraits) != details::dependent_zero<GuardTraits>>>
-inline void on_main(Guard &&guard, Callable &&callable) {
-	on_main([
-		passed_guard = GuardTraits::create(std::forward<Guard>(guard)),
-		passed_callable = std::forward<Callable>(callable)
-	]() mutable {
-		if (GuardTraits::check(passed_guard)) {
-			std::forward<Callable>(passed_callable)();
-		}
-	});
+inline auto guard(Guard &&object, Callable &&callable)
+-> guarded_wrap<Guard, Callable> {
+	return {
+		std::forward<Guard>(object),
+		std::forward<Callable>(callable)
+	};
 }
 
 template <
@@ -47,15 +96,22 @@ template <
 	typename GuardTraits = guard_traits<std::decay_t<Guard>>,
 	typename = std::enable_if_t<
 		sizeof(GuardTraits) != details::dependent_zero<GuardTraits>>>
-inline void on_main_sync(Guard &&guard, Callable &&callable) {
-	on_main_sync([
-		passed_guard = GuardTraits::create(std::forward<Guard>(guard)),
-		passed_callable = std::forward<Callable>(callable)
-	]() mutable {
-		if (GuardTraits::check(passed_guard)) {
-			std::forward<Callable>(passed_callable)();
-		}
-	});
+inline void on_main(Guard &&object, Callable &&callable) {
+	return on_main(guard(
+		std::forward<Guard>(object),
+		std::forward<Callable>(callable)));
+}
+
+template <
+	typename Guard,
+	typename Callable,
+	typename GuardTraits = guard_traits<std::decay_t<Guard>>,
+	typename = std::enable_if_t<
+		sizeof(GuardTraits) != details::dependent_zero<GuardTraits>>>
+inline void on_main_sync(Guard &&object, Callable &&callable) {
+	return on_main_sync(guard(
+		std::forward<Guard>(object),
+		std::forward<Callable>(callable)));
 }
 
 } // namespace crl
