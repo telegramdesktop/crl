d259aebc11df52cb6ff8c738580dc4d8f245d681
John Preston <johnprestonmail@gmail.com>
2019-05-28 13:37:19 +0200

    Allow hooking to main repaints using crl.

--- src/crl/crl_on_main.h
+++ src/crl/crl_on_main.h
@@ -22,3 +22,15 @@ https://github.com/telegramdesktop/tdesktop/blob/master/LEGAL
 #include <crl/common/crl_common_on_main_guarded.h>
 #include <crl/common/crl_common_guards.h>
 #include <crl/qt/crl_qt_guards.h>
+
+#ifdef CRL_ENABLE_RPL_INTEGRATION
+
+#include <rpl/producer.h>
+
+namespace crl {
+
+rpl::producer<> on_main_update_requests();
+
+} // namespace crl
+
+#endif // CRL_ENABLE_RPL_INTEGRATION
